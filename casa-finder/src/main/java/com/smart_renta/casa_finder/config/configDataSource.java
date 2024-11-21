a4c66436a3ded045fe28ad1e8e20f9c12c133af1
grecia <greciaguerreroa@gmail.com>
2024-11-20 23:54:09 -0500

    add docker

---
+++ casa-finder/src/main/java/com/smart_renta/casa_finder/config/configDataSource.java
@@ -0,0 +1,28 @@
+package com.smart_renta.casa_finder.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class configDataSource {
+
+    @Value("${DB_USERNAME}")
+    private String DBUsername;
+    @Value("${DB_PASSWORD}")
+    private String DBPassword;
+    @Value("${DB_URL}")
+    private String DBUrl;
+
+    @Bean
+    public DataSource getDataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(DBUrl);
+        dataSourceBuilder.username(DBUsername);
+        dataSourceBuilder.password(DBPassword);
+        return dataSourceBuilder.build();
+    }
+}
